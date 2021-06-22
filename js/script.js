381eaf40c73a7cf5f67bc486032743d905b33082
yusuftravlr <yusuf.travlr@gmail.com>
2021-06-22 15:57:42 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -14,10 +14,8 @@ $(".page-scroll").on("click", function (event) {
   console.log(intTarget);
 });
 
-//Get the button
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
@@ -29,7 +27,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
+
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
