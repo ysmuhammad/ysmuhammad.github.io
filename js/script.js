549da98ae1214731d4800e222e0f698a5c7f3ed6
yusuftravlr <yusuf.travlr@gmail.com>
2021-06-22 15:57:02 +0700

    update

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(".page-scroll").on("click", function (event) {
   }
   var val = $(this).attr("href");
   var valF = $(val);
-  var targetOffset = $(valF).offset().top - 120;
+  var targetOffset = $(valF).offset().top - 80;
   var intTarget = parseInt(targetOffset);
   //   $("html, body").animate({
   //     scrollTop: 2000,
@@ -13,3 +13,26 @@ $(".page-scroll").on("click", function (event) {
   event.preventDefault();
   console.log(intTarget);
 });
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
